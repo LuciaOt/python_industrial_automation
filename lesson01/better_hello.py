176ad12f21fa0a3f8791a9ee79c6edd9fc5609b2
Ondrej Kyjanek <ondrej.kyjanek@email.cz>
2025-09-23 16:37:43 +0200

    execution time

--- lesson01/better_hello.py
+++ lesson01/better_hello.py
@@ -1,10 +1,14 @@
 from progress.bar import Bar
 import time
 
+start_t = time.time()
 bar = Bar("Generating hello", max=20)
 for i in range(20):
     # Simulate work being done
-    time.sleep(0.5)
+    time.sleep(0.25)
     bar.next()
 bar.finish()
 print("Hello, world!")
+
+end_t = time.time()
+print(f"Execution time: {end_t - start_t} seconds")
